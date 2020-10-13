015bff5080364664367ab227b6613f4b49822afc
fcobarbosa <fcobarbosa2006@hotmail.com>
2020-10-13 07:03:10 -0300

    Arquivos JS e CSS Configurações do Bundle

--- src/DevIO.UI.Site/Startup.cs
+++ src/DevIO.UI.Site/Startup.cs
@@ -27,6 +27,8 @@ namespace DevIO.UI.Site
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseMvc(routes => 
             {
                 routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
